dbe4ee80d01eb77ebaf22e7a44f80427a7f66ccb
Seth Kranzler <sethkranzler@gmail.com>
2017-04-03 15:32:58 -0400

    fixes performance alignment

---
+++ _includes/main.js
@@ -0,0 +1,13 @@
+// function toArray(el){
+//   var arr = [];
+//   for (var i = 0; i < el.length; i ++){
+//     arr[i] = el[i];
+//   }
+//   return(arr);
+// }
+
+// function marquee(){
+
+// }
+
+// var marquees = toArray(document.getElementsByClassName("marquee")); //.getElementsByTagName("li");
